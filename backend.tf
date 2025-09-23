7b5f3148ec4d91763cfd6b183d3ae5a3976f128b
chenna333 <59122294+chenna333@users.noreply.github.com>
2025-09-23 07:58:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "YOUR-S3-BUCKET-NAME"   # replace later
+    bucket         = "chennareddymalireddy-demo"   # replace later
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"       # optional, for state locking
